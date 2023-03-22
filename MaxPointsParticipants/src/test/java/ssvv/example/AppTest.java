e0b9b34ca77cdcf8cde20e7e029b047184d710ad
915-Murariu-Alexandra <nymeriankida@yahoo.com>
2023-03-22 12:39:14 +0200

    added test cases for negative id and string id

---
+++ MaxPointsParticipants/src/test/java/ssvv/example/AppTest.java
@@ -0,0 +1,62 @@
+package ssvv.example;
+
+import domain.Student;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    @Test
+    public void addStudentEmptyId() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        try{
+            service.addStudent(new Student("-1", "aaa", 935, "aaa@aaa.com"));
+            fail();
+        } catch(ValidationException e) {
+            return;
+        }
+    }
+
+    @Test
+    public void addStudentValidId() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        try{
+            service.addStudent(new Student("aaa", "aaa", 935, "aaa@aaa.com"));
+            fail();
+        } catch(ValidationException e) {
+            return;
+        }
+    }
+
+}
