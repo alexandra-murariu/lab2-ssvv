55356640ad431bbdd4440f9c9bfb8e48ae957096
915-Murariu-Alexandra <nymeriankida@yahoo.com>
2023-04-26 14:35:02 +0300

    added 2 tests

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,5 +1,6 @@
 package ssvv.example;
 
+import domain.Nota;
 import domain.Student;
 import domain.Tema;
 import junit.framework.TestCase;
@@ -12,7 +13,7 @@ import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.ValidationException;
-
+import java.time.LocalDate;
 import static org.junit.Assert.*;
 
 /**
@@ -273,9 +274,9 @@ public class AppTest extends TestCase {
         NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
         Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
 
-        service.addStudent(new Student("1212", "aaa", 0, "aaa"));
-        assertTrue(service.findStudent("1212") != null);
-        service.deleteStudent("1212");
+        service.addStudent(new Student("12222", "aaa", 0, "aaa"));
+        assertTrue(service.findStudent("12222") != null);
+        service.deleteStudent("12222");
 
     }
 
@@ -400,11 +401,62 @@ public class AppTest extends TestCase {
 
         try {
             service.addTema(new Tema("1", "aaa", 1, 1));
+        } catch (ValidationException e) {
+            return;
+        }
+
+    }
+
+    @Test
+    public void addGradeInvalidNota() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        String data = "2023-12-12";
+        String[] date = data.split("-");
+        LocalDate dataPredare = LocalDate.of(Integer.parseInt(date[0]), Integer.parseInt(date[1]), Integer.parseInt(date[2]));
+        try {
+            service.addNota(new Nota("1", "1", "1", 11.00, dataPredare), "feedback");
             fail();
         } catch (ValidationException e) {
             return;
         }
 
     }
+    @Test
+    public void integrationAddStudentAssignmentGrade() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        String data = "2023-05-05";
+        String[] date = data.split("-");
+        LocalDate dataPredare = LocalDate.of(Integer.parseInt(date[0]), Integer.parseInt(date[1]), Integer.parseInt(date[2]));
+        try {
+            service.addStudent(new Student("558", "bbb", 935, "aaa@aaa.com"));
+            service.addTema(new Tema("12", "aaa", 8, 6));
+            service.addNota(new Nota("1", "557", "12", 9.00, dataPredare), "feedback");
+        } catch (ValidationException e) {
+            fail();
+            return;
+        }
+        service.deleteStudent("558");
+        service.deleteTema("12");
+        service.deleteNota("1");
+    }
+
 
 }
